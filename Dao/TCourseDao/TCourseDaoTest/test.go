28b0f846e9192cb0ebcb5fb42720205542d864cb
oceanct <yangzhichao_hit@163.com>
2022-02-03 16:59:47 +0800

    correct dao

--- Dao/TCourseDao/TCourseDaoTest/test.go
+++ Dao/TCourseDao/TCourseDaoTest/test.go
@@ -2,17 +2,27 @@
 package main
 
 import (
-	"Project1/Dao/TCourseDao"
-	"github.com/ByteDanceBackendProject18/Project/Types"
+	"Project1/Dao/DBAccessor"
+	//"github.com/ByteDanceBackendProject18/Project/Dao/DBAccessor"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func TestInsertCourse() {
-	var course Types.TCourse
-	course.CourseID = string(rune(0x0000))
-	course.Name = "Goland Backend"
-	TCourseDao.InsertCourse(course)
-}
+//func TestInsertCourse() {
+//	var course Types.TCourse
+//	course.CourseID = string(rune(0x0000))
+//	course.Name = "Goland Backend"
+//	TCourseDao.InsertCourse(course)
+//}
 func main() {
-
+	DBAccessor.MySqlInit()
+	//db, err := DBAccessor.MySqlInit()
+	//defer func(db *gorm.DB) {
+	//	_ = db.Close()
+	//}(db)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	fmt.Println("Database connection refused.")
+	//} else {
+	//	// 直到建表成功才继续
+	//}
 }
